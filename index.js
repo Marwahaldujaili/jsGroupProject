6f774271402f9ecefeb51d57b5609dfca578e609
Elena <elenarmaroli@gmail.com>
2023-04-17 15:53:18 +0200

    first day

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 
 class Calculator {
     constructor() {
-        //properties
-        PI = 3.141592653589793,
-            E = 2.718281828459045
+    //properties
+        this.PI = 3.141592653589793,
+        this.E = 2.718281828459045
     }
 
     //getter
-get pi() {
-    return `${this.PI}`
-}
+    get pi() {
+        return `PI (${this.PI})`;
+    }
+    get e() {
+        return `Euler's number (${this.E})`;
+    }
     //methods
 }
 
-console.log(Calculator.pi())
+const calculator1 = new Calculator;
+
+console.log(calculator1.pi);
+console.log(calculator1.e);
+console.log(calculator1);
