655aed3c01b91ab8a18d4121716f0528da64eefe
MarwahalDujaili <aldujailimarwa@gmail.com>
2023-04-17 15:31:25 +0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
-class Calculator{
-    constructor(){
+class Calculator {
+    constructor() {
         //properties
+        PI = 3.141592653589793,
+            E = 2.718281828459045
     }
 
     //getter
-    
+get pi() {
+    return `${this.PI}`
+}
     //methods
 }
+
+console.log(Calculator.pi())
